4dc1f8ade8580e113fc9a4330fbd21856bdd4bb1
Wiki <oscar_perez@windowlive.com>
2022-11-08 21:24:13 -0600

    New changes

---
+++ headers.py
@@ -0,0 +1,94 @@
+import random
+
+
+class HEADERS():
+
+				def header_selector(self):
+								header = {
+												"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+												"Accept-Encoding": "gzip, deflate, br",
+												"Accept-Language": "en-US,en;q=0.9,es;q=0.8",
+												"Sec-Ch-Ua": "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"",
+												"Sec-Ch-Ua-Platform": "\"Windows\"",
+												"User-Agent": "{}",
+								}
+
+
+user_agents = (
+				{
+								"Chrome": {
+												"Chrome 74 on Windows 10": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+												"Chrome 72 on Windows 10": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+												"Chrome 44 on Linux": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+								},
+								"Chromium": {
+												"Chromium 74 on Linux": "Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Raspbian Chromium/74.0.3729.157 Chrome/74.0.3729.157 Safari/537.36",
+								},
+								"Firefox": {
+												"Firefox 33 on Mac OS X (Yosemite)": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0",
+												"Firefox 24 on Ubuntu Linux": "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:24.0) Gecko/20100101 Firefox/24.0",
+												"Firefox 54 on Windows 7": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+												"Firefox 7 on Windows XP": "Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1",
+								},
+								"Opera": {
+												"Opera 12 on Windows 7": "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.18",
+												"Opera 84 on Windows 10": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36 OPR/84.0.4316.14",
+												"Opera 12 on Linux": "Opera/9.80 (Linux armv7l) Presto/2.12.407 Version/12.51 , D50u-D1-UHD/V1.5.16-UHD (Vizio, D50u-D1, Wireless)",
+								},
+								"Safari": {
+												"Safari 11.1 on Mac OS X (El Capitan)": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15",
+												"Safari 10.1 on Mac OS X (Yosemite)": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
+								},
+								"Apple iPhone/iPad": {
+												"Apple iPad iOS 12.2": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+												"Apple iPhone iOS 12.2": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+								},
+								"Internet Explorer": {
+												"Internet Explorer 6 on Windows XP SP2": "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)",
+												"Internet Explorer 11 on Windows 7": "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+								},
+								"Outlook 3": {
+												"Outlook 3 on iOS Apple iPhone": "Outlook-iOS/709.2226530.prod.iphone (3.24.1)",
+								},
+								"Thunderbird": {
+												"Thunderbird 45.8 on Linux": "Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Thunderbird/45.8.0",
+								}
+				}
+)
+
+user_agent_software = {
+				"1": "Chrome",
+				"2": "Chromium",
+				"3": "Firefox",
+				"4": "Opera",
+				"5": "Safari",
+				"6": "Apple iPhone/iPad",
+				"7": "Internet Explorer",
+				"8": "Outlook 3",
+				"9": "Thunderbird"
+}
+
+
+# uas = input(str("Select: "))
+# if uas in user_agent_software.keys():
+# 				print(user_agent_software[uas])
+# else:
+# 				print("False")
+
+def show_user_agent_software():
+				for software in user_agent_software:
+								print(software, user_agent_software[software])
+
+
+def user_agent_selector(software="Thunderbird", values=False):
+				for user_agent in user_agents[software]:
+								if values:
+												print(user_agent, "--->", user_agents[software][user_agent])
+								else:
+												print(user_agent)
+
+
+def random_user_agent():
+				software = random.choice(list(user_agent_software.values()))
+				user_agent = random.choice(list(user_agents[software].values()))
+				print(user_agent)
--- link_extractor.py
+++ link_extractor.py
@@ -1,5 +1,4 @@
 from utils.alerts import good, bad
-from utils.headers import HEADERS
 from time import sleep
 import requests
 import database
@@ -8,83 +7,32 @@ import json
 from bs4 import BeautifulSoup
 import colorama
 
-def run_enumeration(target_host):
+SITE = "https://lacatedralmusical.com"
 
-    try:
-        print(good, 'Requesting domain {}'.format(target_host))
-        sleep(.5)
 
-        requests_call = requests.get(target_host, headers=HEADERS)
-        html_object = requests_call.content.__str__()
 
-        print(good, 'Domains extraction start...')
-        sleep(.5)
-        data = re.findall(r'(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?', html_object)
 
-        malicious_string = ['xxx', 'porn', 'sex', 'xnxx', 'xvideo', '.firebaseio.com']
+# def run_enumeration(target_host):
+def links_extractor(target_host):
+				try:
+								print(good, 'Requesting domain {}'.format(target_host))
+								sleep(.5)
+								requests_call = requests.get(target_host, headers=HEADERS)
+								html_object = requests_call.content.__str__()
+								print(good, 'Domains extraction start...')
+								sleep(.5)
+								data = re.findall(r'(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?', html_object)
+								print(data)
 
-        print(good, 'Domain filtering...')
-        sleep(.5)
+				except (requests.exceptions.ConnectionError, requests.exceptions.MissingSchema) as err:
+								e = err.__str__()
+								if e[:18] == 'HTTPConnectionPool' or e[:19] == 'HTTPSConnectionPool':
+												print(bad, 'Failed to establish a new connection, domain not available!')
+								elif e[2:20] == 'Connection aborted':
+												print(bad, 'An existing connection has been forced to break by the remote host')
+								else:
+												print(bad, e)
 
-        positive_domains = []
-        non_positive_domains = []
 
-        for url_data in data:
-            trigger = None
-            protocol = url_data[0]
-            domain = url_data[1]
-            route = url_data[2]
 
-            for string in malicious_string:
-                if string in domain + route:
-                    positive_domains.append(domain + route)
-                    trigger = True
-                    if domain[:4] == 'www.':
-                        print(bad, domain[4:] + route)
-                        sleep(.3)
-                    else:
-                        print(bad, domain + route)
-                        sleep(.3)
-                else:
-                    pass
-            if trigger == None:
-                non_positive_domains.append(domain + route)
-                if domain[:4] == 'www.':
-                    print(good, domain[4:] + route)
-                    sleep(.3)
-                else:
-                    print(good, domain + route)
-                    sleep(.3)
 
-        json_data = {
-            target_host : {
-                'positive_domains': positive_domains,
-                'non_positive_domains': non_positive_domains
-                }
-            }
-
-        #   Data Base Function
-        database.write_database(json_data)
-        print('Data Base saved!')
-
-        sleep(1)
-        if len(positive_domains) > 0:
-            print("""
-            Palpe0 found {} positive domains
-            Palpe0 found {} non positive domains
-            """.format(len(positive_domains), len(non_positive_domains)))
-        else:
-            print('All domains checked, {} domains non positive'.format(len(non_positive_domains)))
-
-        print(good, 'done!')
-    except (requests.exceptions.ConnectionError, requests.exceptions.MissingSchema) as err:
-        e = err.__str__()
-        if e[:18] == 'HTTPConnectionPool' or e[:19] == 'HTTPSConnectionPool':
-            print(bad, 'Failed to establish a new connection, domain not available!')
-        elif e[2:20] == 'Connection aborted':
-            print(bad, 'An existing connection has been forced to break by the remote host')
-        else:
-            print(bad, e)
-    except KeyboardInterrupt:
-        print()
-        print('Palpe0 Stopped...')
